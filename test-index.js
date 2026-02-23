dabf4615dfffbe8b63cd8f66aa0e36889bcbfe75
ART <business.with.art33@gmail.com>
2026-02-24 00:00:13 +0100

    Update test-index.js
    Add event listener to toggle visibility of answers

--- test-index.js
+++ test-index.js
@@ -1,1 +1,8 @@
+'use strict';
 
+
+
+document.querySelector('.arrow-1').addEventListener('click', function () {
+  document.querySelector('.answer-1').classList.add('hidden')
+  document.querySelector('.answer').classList.remove('hidden')
+});
