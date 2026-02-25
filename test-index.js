2150b0428f9e557b7c2d23e6ee2e42af0cc18a77
ART <business.with.art33@gmail.com>
2026-02-25 23:50:24 +0100

    Update test-index.js
    Added event listeners for multiple arrows and questions.

--- test-index.js
+++ test-index.js
@@ -3,4 +3,49 @@
 
 
 document.querySelector('.arrow-1').addEventListener('click', function () {
+  document.querySelector('.click-question-1').addEventListener('click', function () {
+
+  document.querySelector('.arrow-1').classList.toggle('rotate-arrow-1')
+  document.querySelector('.answer-1').classList.toggle('hidden-answer-1')
+  });
+});
+
+
+
+document.querySelector('.arrow-2').addEventListener('click', function () {
+  document.querySelector('.click-question-2').addEventListener('click', function () {
+
+    document.querySelector('.arrow-2').classList.toggle('rotate-arrow-2')
+    document.querySelector('.answer-2').classList.toggle('hidden-answer-2')
+  });
+});
+
+
+
+document.querySelector('.arrow-3').addEventListener('click', function () {
+  document.querySelector('.click-question-3').addEventListener('click', function () {
+
+    document.querySelector('.arrow-3').classList.toggle('rotate-arrow-3')
+    document.querySelector('.answer-3').classList.toggle('hidden-answer-3')
+  });
+});
+
+
+
+document.querySelector('.arrow-4').addEventListener('click', function() {
+  document.querySelector('.click-question-4').addEventListener('click', function () {
+
+    document.querySelector('.arrow-4').classList.toggle('rotate-arrow-4')
+    document.querySelector('.answer-4').classList.toggle('hidden-answer-4')
+  });
+});
+
+
+
+
+
+
+document.querySelectorAll('.arrows-all').addEventListener('click', function () {
+  document.querySelectorAll('.questions-all').addEventListener('click', function() {
+  });
 });
